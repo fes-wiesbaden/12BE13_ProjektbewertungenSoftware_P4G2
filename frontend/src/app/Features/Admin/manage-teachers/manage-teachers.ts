7dba3414a4d7adaf1c0e68562686f548d66c3a30
Laurin Schubert <Laurin.Schubert@sva.de>
2025-12-05 07:48:49 +0100

    fix: change naming

--- frontend/src/app/Features/Admin/manage-teachers/manage-teachers.ts
+++ frontend/src/app/Features/Admin/manage-teachers/manage-teachers.ts
@@ -10,7 +10,7 @@ import {
   TableColumnComponent,
 } from '../../../Shared/Components/table-column/table-column';
 import { FormField, FormModalComponent } from '../../../Shared/Components/form-modal/form-modal';
-import { DeleteButton } from "../../../Shared/Components/delete-button/delete-button";
+import { DeleteButtonComponent } from "../../../Shared/Components/delete-button/delete-button";
 
 @Component({
   selector: 'app-manage-teachers',
@@ -22,7 +22,7 @@ import { DeleteButton } from "../../../Shared/Components/delete-button/delete-bu
     PageHeaderComponents,
     TableColumnComponent,
     FormModalComponent,
-    DeleteButton
+    DeleteButtonComponent
 ],
   templateUrl: './manage-teachers.html',
 })
