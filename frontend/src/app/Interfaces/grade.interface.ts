d0ca29aec919f02b437196d25af9a5638d65eff1
Sebastian <145010047+sebiwsk@users.noreply.github.com>
2025-12-05 14:02:22 +0100

    refactor: own interface for grade and learning-field

---
+++ frontend/src/app/Interfaces/grade.interface.ts
@@ -0,0 +1,6 @@
+export interface Grade {
+  id: string;
+  value: number;
+  gradeWeighting: number;
+  date: string;
+}
--- frontend/src/app/Interfaces/learning-field.interface.ts
+++ frontend/src/app/Interfaces/learning-field.interface.ts
@@ -2,12 +2,4 @@ export interface LearningField {
   id: string;
   name: string;
   weighting: number;
-  grades: Grade[];
-}
-
-export interface Grade {
-  id: string;
-  value: number;
-  gradeWeighting: number;
-  date: string;
 }
