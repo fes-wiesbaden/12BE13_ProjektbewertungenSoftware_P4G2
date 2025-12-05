1303570091d22c1977234ed87bdbba1d43d747d2
Laurin Schubert <Laurin.Schubert@sva.de>
2025-12-05 11:48:20 +0100

    feat: add interface and service to learnfield

---
+++ frontend/src/app/Features/Admin/manage-learningfields/learningfields.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AddLearningfields, LearningField } from '../../../Interfaces/learningfields.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class learningfieldService {
+  private apiUrl = 'http://localhost:4100/api/question';
+
+  constructor(private http: HttpClient) {}
+
+  getLearningfields(): Observable<LearningField[]> {
+    return this.http.get<LearningField[]>(this.apiUrl);
+  }
+
+  createLearningfields(dto: AddLearningfields): Observable<LearningField> {
+    return this.http.post<LearningField>(this.apiUrl, dto);
+  }
+
+  updateLearningfields(dto: LearningField): Observable<LearningField> {
+    return this.http.put<LearningField>(`${this.apiUrl}/${dto.id}`, dto);
+  }
+}
---
+++ frontend/src/app/Interfaces/learningfields.interface.ts
@@ -0,0 +1,8 @@
+export interface AddLearningfields {
+  learningfieldText: string;
+}
+
+export interface LearningField {
+  id: string;
+  learningfieldText: string;
+}
