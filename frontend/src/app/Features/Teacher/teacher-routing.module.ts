4855a25ada010a266bca5d50d68d165d4107ae8f
Sebastian <145010047+sebiwsk@users.noreply.github.com>
2025-12-06 14:15:41 +0100

    remove: unused manage-grades

--- frontend/src/app/Features/Teacher/teacher-routing.module.ts
+++ frontend/src/app/Features/Teacher/teacher-routing.module.ts
@@ -3,7 +3,6 @@ import { TeacherDashboard } from './teacher-dashboard/teacher-dashboard';
 import { MyStudents } from './my-students/my-students';
 import { MyProfile } from './my-profile/my-profile';
 import { StudentGroups } from "./student-groups/student-groups";
-import { ManageGrades } from "./manage-grades/manage-grades";
 import { Projects } from "./projects/projects";
 import { Project } from "./project/project";
 import { Group } from "./group/group";
@@ -26,10 +25,6 @@ export const TeacherRoutes: Routes = [
   {
     path: 'manage-learning-fields/:studentId',
     component: ManageLearningField
-  },
-  {
-    path: 'manage-grades',
-    component: ManageGrades
   },
   {
     path: 'groups',
