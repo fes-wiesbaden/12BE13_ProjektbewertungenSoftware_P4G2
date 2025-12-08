71a25a46952ea2099c82560c61b8332be5bfcd7c
georgdatagraphis <156414420+georgdatagraphis@users.noreply.github.com>
2025-12-08 13:31:02 +0100

    [FEATURE] app.config.ts und token-interceptor.ts angepasst

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,12 +3,15 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { tokenInterceptor } from './core/auth/token-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([tokenInterceptor])
+      ),
   ]
 };
