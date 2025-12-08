e83a7e3403185d0e0c4159d5c061f41beaf2af4c
Laurin Schubert <Laurin.Schubert@sva.de>
2025-12-08 17:11:03 +0100

    feat : add interface for review

---
+++ frontend/src/app/Interfaces/review.interface.ts
@@ -0,0 +1,12 @@
+export interface Review {
+  id: string;
+  projectId: string;
+  userId: string;
+  date: string;
+  review: ReviewJson;
+}
+export interface ReviewJson {
+  questionID: number;
+  questionText: string;
+  students: Array<{ studentID: number; grade: number }>;
+}
