1673bc4aeb85e133d104b434882d1f550f230a1a
georgdatagraphis <156414420+georgdatagraphis@users.noreply.github.com>
2025-12-09 11:07:03 +0100

    [FEATURE] project dockerisiert

--- frontend/src/app/Features/Auth/auth-routing.module.ts
+++ frontend/src/app/Features/Auth/auth-routing.module.ts
@@ -1,11 +1,14 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 import { Login } from './login/login';
 
-
 export const AuthRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
   {
     path: 'login',
     component: Login,
   },
-  
 ];
