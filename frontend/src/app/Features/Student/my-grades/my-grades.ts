a8d30dbab40f968ee2f3503f4cd8b998290dedb3
Laurin Schubert <Laurin.Schubert@sva.de>
2025-12-09 10:51:47 +0100

    feat: add popUp for Grade Overview

--- frontend/src/app/Features/Student/my-grades/my-grades.ts
+++ frontend/src/app/Features/Student/my-grades/my-grades.ts
@@ -8,17 +8,22 @@ import { Grade } from '../../../Interfaces/grade.interface';
 import { LearningField } from '../../../Shared/models/learning-fields.interface';
 import { LearningFieldService } from '../../../Shared/Services/learning-field.service';
 import { AuthService } from '../../../core/auth/auth.service';
+import { AddGradeForm } from "../../../Shared/Components/add-grade-form/add-grade-form";
+import { GradeViewerModalComponent } from "../../../Shared/Components/grade-viewer/grade-viewer";
 
 @Component({
   selector: 'app-my-grades',
-  imports: [PageHeaderComponents, TableColumnComponent],
+  imports: [PageHeaderComponents, TableColumnComponent, AddGradeForm, GradeViewerModalComponent],
   templateUrl: './my-grades.html'
 })
 export class MyGrades {
   userId!: string;
   currentLearningFieldId!: string;
   learningFields: LearningField[] = [];
-  grades: Grade[] = [];
+  myGrades: Grade[] = [
+  { id: '1', gradeName: 'LF 1', value: 5, gradeWeighting: 30 ,date:"2025-12-09T09:13:34.173+00:00"},
+  { id: '2', gradeName: 'LF 2', value: 4, gradeWeighting: 70, date:"2025-12-09T09:13:34.173+00:00" }
+];
   loading = true;
   isAddModalVisible = false;
 
@@ -26,12 +31,18 @@ export class MyGrades {
     { key: 'name', label: 'Lernfeldname' },
     { key: 'weighting', label: 'Gewichtung' },
   ];
+showViewer: any;
 
   constructor(private learningFieldService: LearningFieldService, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.userId = this.authService.getUserId();
     this.loadLearningFields();
+  }
+
+  openAddModal(item: any) {
+    this.loading = true;
+    this.showViewer= true;
   }
 
   loadLearningFields() {
--- frontend/src/app/Shared/Components/grade-viewer/grade-viewer.ts
+++ frontend/src/app/Shared/Components/grade-viewer/grade-viewer.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Grade } from '../../../Interfaces/grade.interface';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-grade-viewer',
+  selector: 'app-grade-viewer-modal',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './grade-viewer.component.html'
+  imports: [CommonModule, MatIcon],
+  templateUrl: './grade-viewer.html',
 })
-export class GradeViewerComponent {
-  @Input() grades: Grade[] = [];
+export class GradeViewerModalComponent {
+  @Input() showModal = false;
   @Input() title: string = 'Notenübersicht';
-}
+  @Input() grades: Grade[] = [];
 
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
+}
