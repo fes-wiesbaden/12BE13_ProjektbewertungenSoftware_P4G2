b752513144bf4e4373dbf62603ba346ae7e11d44
Mahdi Haidary <mahdi.haidary@sinc.de>
2025-12-12 08:31:44 +0100

    get all projects and list them in frontend in teacher view

---
+++ frontend/src/app/core/services/project.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {IProject, ProjectCreateRequestDto, ProjectResponseDto} from '../modals/project.modal';
+import {map, Observable} from 'rxjs';
+import {ProjectMapperService} from './project.mapper.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  private projectsApi = 'http://localhost:4100/api/projects';
+
+  constructor(private http: HttpClient, private mapper: ProjectMapperService) {}
+
+  createProject(project: ProjectCreateRequestDto): Observable<IProject> {
+    return this.http.post<ProjectResponseDto>(`${this.projectsApi}`, project)
+      .pipe(
+        map(dto => this.mapper.dtoToProject(dto))
+      );
+  }
+
+  getProjectById(projectId: string): Observable<IProject> {
+    return this.http.get<ProjectResponseDto>(`${this.projectsApi}/${projectId}`)
+      .pipe(
+        map(dto => this.mapper.dtoToProject(dto))
+      );
+  }
+
+  getAllProjects(): Observable<IProject[]> {
+    return this.http.get<ProjectResponseDto[]>(`${this.projectsApi}`)
+      .pipe(
+        map(dtos => this.mapper.dtosToProjects(dtos))
+      );
+  }
+}
